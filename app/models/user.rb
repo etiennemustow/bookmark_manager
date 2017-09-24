e8a5e705b511eef60269e4d1872bed527b7a6ec4
etiennemustow <etienne.o.mustow@gmail.com>
2017-09-24 15:27:23 +0100

    User can sign in with correct credentials. User model spec created

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,16 @@ class User
 
   validates_confirmation_of :password
 
+  def self.authenticate(email, password)
+     user = first(email: email)
+     if user && BCrypt::Password.new(user.password_digest) == password
+       user
+     else
+       nil
+     end
+   end
+
+
 
 
 end
